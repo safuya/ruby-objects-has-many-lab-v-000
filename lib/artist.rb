394f93d5c413a9c0b9fb98fd46150a248a3723ea
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 08:20:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,8 @@ class Artist
     @songs << song
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
     add_song(song)
   end
 
