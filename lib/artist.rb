d52699e2634da4a8643fe8ea5f7bae5c3073c8d2
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-27 08:36:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,24 +2,26 @@ require 'pry'
 
 class Artist
   attr_accessor :name, :songs
+  @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = []
   end
 
+  def self.all_songs
+    @@song_count
+  end
+
   def add_song(song)
     song.artist = self
+    self.all_songs(song)
     @songs << song
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     add_song(song)
-  end
-
-  def self.song_count
-    Song.all.length
   end
 
 end
